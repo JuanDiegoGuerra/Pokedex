6d883bec562f4d4a4fdf5875f60cf73a9a470db2
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-04 21:43:01 -0500

    Update script.js
    Added constant variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const pokeCard = document.querySelector('[data-poke-card]');
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+const pokeImgContainer = document.querySelector('[data-poke-img-container]');
+const pokeId = document.querySelector('[data-poke-id]');
+const pokeTypes = document.querySelector('[data-poke-types]');
+const pokeStats = document.querySelector('[data-poke-stats]');
